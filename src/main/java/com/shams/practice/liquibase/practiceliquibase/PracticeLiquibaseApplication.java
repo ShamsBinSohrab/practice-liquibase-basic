5d95104ac72d8260429e36d46053be4733d408ac
ShamsBinSohrab <aniknishan@gmail.com>
2019-12-10 18:25:28 +0600

    added project dependencies and master changelog

---
+++ src/main/java/com/shams/practice/liquibase/practiceliquibase/PracticeLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.shams.practice.liquibase.practiceliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeLiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeLiquibaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shams/practice/liquibase/practiceliquibase/PracticeLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shams.practice.liquibase.practiceliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeLiquibaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
